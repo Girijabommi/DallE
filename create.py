46991f08e2797ee28d04d39a4f03e889c1b98ab1
Girijabommi <126477996+Girijabommi@users.noreply.github.com>
2023-03-03 13:34:15 +0530

    Update create.py

--- create.py
+++ create.py
@@ -4,12 +4,17 @@ from pathlib import Path
 
 import openai
 
+from decouple import config
+
+API_USERNAME = config('USER')
+API_KEY = config('KEY')
+
 PROMPT = "An eco-friendly computer from the 90s in the style of vaporwave"
 DATA_DIR = Path.cwd() / "responses"
 
 DATA_DIR.mkdir(exist_ok=True)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = API_KEY
 
 response = openai.Image.create(
     prompt=PROMPT,
